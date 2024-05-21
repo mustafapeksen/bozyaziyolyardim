6770996ecd535b803f400cb2ff07c95456f59feb
mustafapeksen <mustafapeksen04@gmail.com>
2024-05-21 19:13:04 +0300

    Update Kurumsal.jsx

--- src/component/Kurumsal.jsx
+++ src/component/Kurumsal.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Kurumsal() {
-    return <h1>Kurumsal</h1>
+    return (
+        <div>
+            <h1>Kurumsal</h1>
+            <h2>Kurumsal</h2>
+            <p>...</p>
+            <h3>Kurumsal</h3>
+        </div>)
 }
 
 export default Kurumsal;
