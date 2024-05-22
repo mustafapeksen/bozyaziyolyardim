b2095836dcd1ea68fde3f9c1ac89a190c081eb94
mustafapeksen <mustafapeksen04@gmail.com>
2024-05-22 14:11:16 +0300

    Update FotoGaleri.jsx

--- src/component/FotoGaleri.jsx
+++ src/component/FotoGaleri.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import PhotoGallery from "./PhotoGallery";
 
 function FotoGaleri() {
-    return <h1>Foto Galeri</h1>
+
+    return (
+        <div>
+            <h1>Foto Galeri</h1>
+            <div id="image">
+                <PhotoGallery />
+            </div>
+        </div>)
 }
 
 export default FotoGaleri;
