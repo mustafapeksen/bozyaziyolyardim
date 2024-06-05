7c3d8e1ea98953ddc47d62c6b85288c621120bfc
mustafapeksen <mustafapeksen04@gmail.com>
2024-06-05 17:13:57 +0300

    Update BlogCard.jsx

--- src/component/BlogCard.jsx
+++ src/component/BlogCard.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
+// BlogCard component to display individual blog entries
 function BlogCard(props) {
     return (
         <div className="blog-card">
-            <img src={props.img} alt="Çekici Fotoğrafı" />
+            {/* Display the image */}
+            <img src={props.img} alt="Blog Image" />
+            {/* Display the title */}
             <h3>{props.title}</h3>
+            {/* Display the text */}
             <p>{props.text}</p>
-        </div>);
+        </div>
+    );
 }
 
 export default BlogCard;
