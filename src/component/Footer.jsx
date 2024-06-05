fec26d2b4dbe5f6cb23150f890809bb1f671b2ba
mustafapeksen <mustafapeksen04@gmail.com>
2024-06-05 16:39:50 +0300

    Update Footer.jsx

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -34,10 +34,10 @@ function Footer() {
                     <a href="https://bozyaziyolyardim.com.tr">Bozyazı Yol Yardım</a>
                 </div>
                 <div id="phone-connect">
-                    <a className="whatsapp" aria-label="Chat on WhatsApp" href="https://wa.me/+905316187998">
+                    <a className="whatsapp" aria-label="Chat on WhatsApp" href="https://wa.me/+12345">
                         <img className="whatsapp" alt="Chat on WhatsApp" src="/image/WhatsAppButtonGreenLarge.svg" />
                     </a>
-                    <a className="cellphone" href="tel:+905316187998"><img className="cellphone" src="/image/PhoneCall.png" alt="Phone on Telephone" /></a>
+                    <a className="cellphone" href="tel:+12345"><img className="cellphone" src="/image/PhoneCall.png" alt="Phone on Telephone" /></a>
                 </div>
             </div>
         </footer>);
