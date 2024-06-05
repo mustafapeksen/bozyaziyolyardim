9273fb8f9c6927010ea1e8c930fd38c09c471e2d
mustafapeksen <mustafapeksen04@gmail.com>
2024-06-05 17:13:13 +0300

    Update Blog.jsx

--- src/component/Blog.jsx
+++ src/component/Blog.jsx
@@ -1,37 +1,39 @@
 import React from "react";
 import BlogCard from "./BlogCard";
 
+// Blog component to display blog entries
 function Blog() {
+    // Array of blog content objects
     const blogContents = [{
         title: "Bozyazı Nöbetçi Yol Yardım",
-        text:
-            "Bozyazı Nöbetçi Yol Yardım Aniden meydana gelen araç arızası ya da yol kazası sonucu yerinde teknik destek yardımı bekleyen araçlar için konusunda deneyimli personelimiz en kısa sürede yardım için yola çıkmaktadır.",
+        text: "Bozyazı Nöbetçi Yol Yardım Aniden meydana gelen araç arızası ya da yol kazası sonucu yerinde teknik destek yardımı bekleyen araçlar için konusunda deneyimli personelimiz en kısa sürede yardım için yola çıkmaktadır.",
         img: "https://bozyaziyolyardim.com.tr/wp-content/uploads/2023/09/nobetci-yol-yardim.webp"
     },
     {
         title: "Bozyazı Yol Yardım Firması",
-        text:
-            "Bozyazı Yol Yardım Firması Yol yardım ve yol destek paketlerimizi en uygun avantajlar ile güncelleyerek, araçlar için yolda kalma sorununu bir kabus olmaktan çıkarmayı amaçlıyoruz.",
+        text: "Bozyazı Yol Yardım Firması Yol yardım ve yol destek paketlerimizi en uygun avantajlar ile güncelleyerek, araçlar için yolda kalma sorununu bir kabus olmaktan çıkarmayı amaçlıyoruz.",
         img: "https://bozyaziyolyardim.com.tr/wp-content/uploads/2023/09/yol-yardim-firmasii.webp"
     }, {
         title: "Bozyazı Yol Yardım Fiyatları",
-        text:
-            "Bozyazı Yol Yardım Fiyatları Bozyazı yol yardım Fiyatları, tüm araç sahiplerinin bütçesi de göz önünde bulundurularak oluşturulmuştur. Abartılı ve gerçekçi olmayan hizmet fiyatlarını, firmamız çatısı altında bulamazsınız.",
+        text: "Bozyazı Yol Yardım Fiyatları Bozyazı yol yardım Fiyatları, tüm araç sahiplerinin bütçesi de göz önünde bulundurularak oluşturulmuştur. Abartılı ve gerçekçi olmayan hizmet fiyatlarını, firmamız çatısı altında bulamazsınız.",
         img: "https://bozyaziyolyardim.com.tr/wp-content/uploads/2023/09/yol-yardim-fiyatlari.webp"
     }];
 
-
     return (
         <div>
+            {/* Title section */}
             <div className="title">
                 <h1>Blog</h1>
             </div>
+            {/* Blog grid to display each blog entry */}
             <div className="blog-grid" id="blog-container">
                 {blogContents.map((blogContent, index) => (
-                    <BlogCard key={index} title={blogContent.title} text={blogContent.text} img={blogContent.img} />)
-                )}
+                    // Render a BlogCard for each blog content
+                    <BlogCard key={index} title={blogContent.title} text={blogContent.text} img={blogContent.img} />
+                ))}
             </div>
-        </div>)
+        </div>
+    );
 }
 
 export default Blog;
