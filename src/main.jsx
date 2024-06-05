05adc4b652fe4e3f33b25dd1d1e1bcf7b6434efe
mustafapeksen <mustafapeksen04@gmail.com>
2024-06-05 17:10:10 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./component/App"
+import App from "./component/App";
 import Kurumsal from './component/Kurumsal';
 import BozyaziYolYardim from './component/BozyaziYolYardim';
 import Hizmetlerimiz from './component/Hizmetlerimiz';
@@ -11,46 +11,48 @@ import Iletisim from './component/Iletisim';
 import NotFound from './component/NotFound';
 import HomePage from './component/HomePage';
 
+// Define the routes for the application using createBrowserRouter
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App />,
-    errorElement: <NotFound />,
-    children: [
+    path: "/", // Root path
+    element: <App />, // Main application component
+    errorElement: <NotFound />, // Component to show when route is not found
+    children: [ // Nested routes
       {
-        path: "",
-        element: <HomePage />
+        path: "", // Default route (home page)
+        element: <HomePage /> // HomePage component
       },
       {
-        path: "kurumsal",
-        element: <Kurumsal />
+        path: "kurumsal", // Path for 'Kurumsal' page
+        element: <Kurumsal /> // Kurumsal component
       },
       {
-        path: "bozyazi-yol-yardim",
-        element: <BozyaziYolYardim />
+        path: "bozyazi-yol-yardim", // Path for 'Bozyazi Yol Yardim' page
+        element: <BozyaziYolYardim /> // BozyaziYolYardim component
       },
       {
-        path: "hizmetlerimiz",
-        element: <Hizmetlerimiz />
+        path: "hizmetlerimiz", // Path for 'Hizmetlerimiz' page
+        element: <Hizmetlerimiz /> // Hizmetlerimiz component
       },
       {
-        path: "foto-galeri",
-        element: <FotoGaleri />
+        path: "foto-galeri", // Path for 'Foto Galeri' page
+        element: <FotoGaleri /> // FotoGaleri component
       },
       {
-        path: "blog",
-        element: <Blog />
+        path: "blog", // Path for 'Blog' page
+        element: <Blog /> // Blog component
       },
       {
-        path: "iletişim",
-        element: <Iletisim />
+        path: "iletişim", // Path for 'Iletisim' page
+        element: <Iletisim /> // Iletisim component
       },
     ],
   },
 ]);
 
+// Render the application to the DOM
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router} /> {/* Provide the router to the application */}
   </React.StrictMode>,
 );
