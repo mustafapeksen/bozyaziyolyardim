bd99f14172c372c3671365ceafbbc9b42a9c7937
mustafapeksen <mustafapeksen04@gmail.com>
2024-06-05 17:18:10 +0300

    Update HizmetlerContent.jsx

--- src/component/HizmetlerContent.jsx
+++ src/component/HizmetlerContent.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
+// HizmetlerContent component to display individual service content
 function HizmetlerContent(props) {
-    return (<div id={"hizmetler-" + props.id}>
-        <img src={props.src} alt={props.name} />
-        <h3><a href="/hizmetlerimiz">{props.name}</a></h3>
-    </div>);
+    return (
+        <div id={"hizmetler-" + props.id}>
+            {/* Display the image with the provided source and alt text */}
+            <img src={props.src} alt={props.name} />
+            {/* Display the service name as a link */}
+            <h3>
+                <a href="/hizmetlerimiz">{props.name}</a>
+            </h3>
+        </div>
+    );
 }
 
 export default HizmetlerContent;
